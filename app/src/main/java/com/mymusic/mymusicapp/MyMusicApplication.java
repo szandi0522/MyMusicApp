a251f48b7769604e56e73a2afbc5f4f061a98596
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-18 11:14:12 +0200

    Dagger to use: MyMusicApplication, MyMusicApplicationComponent

---
+++ app/src/main/java/com/mymusic/mymusicapp/MyMusicApplication.java
@@ -0,0 +1,19 @@
+package com.mymusic.mymusicapp;
+
+import android.app.Application;
+
+import com.mymusic.mymusicapp.view.ViewModule;
+
+/**
+ * Created by mobsoft on 2016. 04. 18..
+ */
+public class MyMusicApplication extends Application{
+
+    public static MyMusicApplicationComponent injector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        injector = DaggerMyMusicApplicationComponent.builder().viewModule(new ViewModule(this)).build();
+    }
+}
---
+++ app/src/main/java/com/mymusic/mymusicapp/MyMusicApplicationComponent.java
@@ -0,0 +1,24 @@
+package com.mymusic.mymusicapp;
+
+import com.mymusic.mymusicapp.interactor.InteractorModule;
+import com.mymusic.mymusicapp.model.ModelModule;
+import com.mymusic.mymusicapp.presenter.MainPresenter;
+import com.mymusic.mymusicapp.view.MainActivity;
+import com.mymusic.mymusicapp.view.ViewModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by mobsoft on 2016. 04. 18..
+ */
+@Singleton
+@Component(modules = {ViewModule.class, InteractorModule.class, ModelModule.class})
+public interface MyMusicApplicationComponent {
+    void inject(MainActivity mainActivity);
+
+    void inject(MainPresenter mainPresenter);
+
+//    void inject(StringInteractor stringInteractor);
+}
