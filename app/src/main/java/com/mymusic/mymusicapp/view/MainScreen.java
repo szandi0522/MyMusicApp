350e1949637f8261d503c9bbf26a728dc408712d
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-18 10:39:36 +0200

    Gradle added dependecies: dagger, sugarORM

--- app/src/main/java/com/mymusic/mymusicapp/view/MainScreen.java
+++ app/src/main/java/com/mymusic/mymusicapp/view/MainScreen.java
@@ -3,5 +3,5 @@ package com.mymusic.mymusicapp.view;
 /**
  * Created by mobsoft on 2016. 04. 18..
  */
-public interface NewSongView {
+public interface MainScreen {
 }
--- app/src/main/java/com/mymusic/mymusicapp/view/NewSongScreen.java
+++ app/src/main/java/com/mymusic/mymusicapp/view/NewSongScreen.java
@@ -3,5 +3,5 @@ package com.mymusic.mymusicapp.view;
 /**
  * Created by mobsoft on 2016. 04. 18..
  */
-public interface MainView {
+public interface NewSongScreen {
 }
