eac6b1ae0d856f06ffb086f2fdd121cebbd637a8
szandi0522 <mezei.alexandra522@gmail.com>
2016-04-28 14:09:56 +0200

    OrmModel and MockModel classes added to the diff models.

---
+++ app/src/main/java/com/mymusic/mymusicapp/model/orm/OrmModel.java
@@ -0,0 +1,13 @@
+package com.mymusic.mymusicapp.model.orm;
+
+import com.mymusic.mymusicapp.model.SongDetails;
+
+import java.util.List;
+
+/**
+ * Created by szandi0522 on 2016.04.28..
+ */
+public class OrmModel {
+
+    List<SongDetails> songList;
+}
