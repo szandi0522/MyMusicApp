5effd0ed46668540365e1047565d871a5630de97
szandi0522 <mezei.alexandra522@gmail.com>
2016-05-14 19:23:38 +0200

    Orm db methods implemented.

--- app/src/main/java/com/mymusic/mymusicapp/model/prod/OrmDbModel.java
+++ app/src/main/java/com/mymusic/mymusicapp/model/prod/OrmDbModel.java
@@ -2,6 +2,7 @@ package com.mymusic.mymusicapp.model.prod;
 
 import com.mymusic.mymusicapp.model.SongDetails;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,19 +15,31 @@ public class OrmDbModel {
     public OrmDbModel(){}
 
     public List<SongDetails> listSongDetails(String text) {
-        List<SongDetails> values = SongDetails.listAll(SongDetails.class);
+        List<SongDetails> values = new ArrayList<SongDetails>();
+        if(text == "") {
+            values = SongDetails.listAll(SongDetails.class);
+        }else{
+            values = SongDetails.find(SongDetails.class, "title = ? or artist = ?", text, text);
+        }
         return values;
     }
     public String addSong(SongDetails song){
-        return "";
+        song.save();
+        return "Song was added to the db!";
     }
 
     public String deleteSong(int id){
-        return "";
+        SongDetails song = SongDetails.findById(SongDetails.class, id);
+        song.delete();
+        return "Song was deleted from the db!";
     }
 
     public String modifyListItem(SongDetails song){
-        return "";
+        SongDetails updatedSong = SongDetails.findById(SongDetails.class, song.getId());
+        updatedSong = song;
+        updatedSong.save();
+
+        return "Song was modified in the db!";
     }
 
 }
